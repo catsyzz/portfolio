b2c617fe0ef1badd3a300a272fbf809a46415e46
Dom Lopez <thedomanater10@gmail.com>
2025-10-22 14:07:19 -0700

    config changes for error messages in site deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,48 +1,28 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
- 
-
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Dom's Portfolio",
   tagline: '',
-
   favicon: 'img/deweyd.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
-  url: 'https://catsyzz.me', // Your website URL
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/portfolio',
+  url: 'https://catsyzz.me',
+  baseUrl: '/', // Change to '/' if deploying to root of custom domain
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'catsyzz', // Usually your GitHub org/user name.
-  projectName: 'portfolio', // Usually your repo name.
+  organizationName: 'catsyzz',
+  projectName: 'portfolio',
   trailingSlash: false,
   deploymentBranch: 'gh-pages',
 
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+    
 
-  // onBrokenLinks: 'throw',
-  // onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -55,19 +35,15 @@ const config = {
       ({
         docs: false,
         blog: {
-          routeBasePath: '/design-processes', // Serve the blog at the site's root
+          routeBasePath: '/design-processes',
           showReadingTime: true,
-          //showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -82,7 +58,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: '/deweydd.png',
       navbar: {
         title: ' ',
@@ -91,21 +66,14 @@ const config = {
           src: '/img/deweydd.png',
         },
         items: [
-
-           {to: '/projects', label: 'Projects', position: 'left'},
-
-          //  {to :'/design-processes', label: 'Design Processes', position: 'left'},
-
-           {to: '/about-me', label: 'About Me', position: 'left'},
+          {to: '/projects', label: 'Projects', position: 'left'},
+          {to: '/about-me', label: 'About Me', position: 'left'},
         ],
       },
-
       footer: {
-        
         copyright: `© Dominic Lopez ${new Date().getFullYear()}`,
       },
       prism: {
-        //theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
     }),
